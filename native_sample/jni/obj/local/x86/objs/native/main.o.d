9918d0e519691ff3556905bef40d1e5eb1b2bbb0
VLC Android <buildbot@videolan.org>
2021-05-14 07:19:35 +0000

    compile 4 all arch

---
+++ native_sample/jni/obj/local/x86/objs/native/main.o.d
@@ -0,0 +1,43 @@
+jni/obj/local/x86/objs/native/main.o: jni/main.c \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/vlc.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_renderer_discoverer.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_picture.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_player.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_list.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_list_player.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_library.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_discoverer.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_events.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_dialog.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_vlm.h \
+  /home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/deprecated.h
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/vlc.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_renderer_discoverer.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_picture.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_player.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_list.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_list_player.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_library.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_media_discoverer.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_events.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_dialog.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/libvlc_vlm.h:
+
+/home/proj/libvlc-android-samples/vlc-android/vlc/include/vlc/deprecated.h:
